76b47e67b94c2c145be99c5021aaffdb0650c00d
msolorzanopardo-cell <msolorzanopardo@gmail.com>
2026-09-19 10:20:25 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
    Al actualizar index.html o admin.html ya no tienes que volver a pegar nada. */
 window.CONFIG = {
   // URL de tu Google Apps Script (termina en /exec)
-  ENDPOINT: "",
+  ENDPOINT: "https://script.google.com/macros/s/AKfycbzFhWEUzjusg7Hp291X9jD2TB_9XJ_HyIpdIdfpbuW4iMtAAxVY3FQ-xzd6njVQ4zBk/exec",
   // Link general de agendamiento (se usa si una vacante no tiene el suyo). Debe empezar con https://
   AGENDA_LINK: "",
   // Opcional: link a tu política de tratamiento de datos
